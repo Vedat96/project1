c4bb3f6c28269d1e1fa7b9135b6f38d27b41a97a
vedat <2051893@talnet.nl>
2020-09-09 11:20:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+login form: 2 input fields met correcte type voor username en password.
+een login knop
+een link met de mogelijkheid een account te maken voor nieuwe gebruikers
+lost password link om je wachtwoord opnieuw op te vragen.
+
+<div class="container">
+    <label for="username"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="username" required>
+
+    <label for="password"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+
+    <button type="submit">Login</button>
+</div>
