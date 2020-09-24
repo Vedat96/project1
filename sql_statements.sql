dc1b78473467dda90908c0beae8031f284a0e76e
vedat <2051893@talnet.nl>
2020-09-24 09:54:51 +0200

    commit message

--- sql_statements.sql
+++ sql_statements.sql
@@ -2,16 +2,16 @@
 create database project1;
 -- maak tabellen aan
 	CREATE TABLE ACCOUNT(
-		ID int not null auto_increment ,
-		email varchar(255) UNIQUE,
-		password varchar(255),
+		ID int not null auto_increment,
+		email varchar(255) not null UNIQUE,
+		password varchar(255) not null,
 		PRIMARY KEY(ID)
 	);
 
 	CREATE TABLE PERSOON(
 		ID int not null auto_increment PRIMARY KEY,
-		voornaam varchar(255),
-		achternaam varchar(255),
+		voornaam varchar(255) not null,
+		achternaam varchar(255) not null,
 		PRIMARY KEY(ID),	
 		FOREIGN KEY (ID) REFERENCES ACCOUNT(ID)
 	);
